15c3429d6f77444753cbe522ce394ea780a88ff4
Hossein <mhosseink1380@gmail.com>
2020-03-13 00:47:06 +0330

    add persian folder

---
+++ persian/assets/js/home.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    const wow = new WOW().init();
+
+    $("#progressbar").progressbar({
+        value: 67
+    });
+
+
+    $("#datepicker").datepicker();
+
+   
+
+})
