fee95a76772c2c0bda06d8b3d98dea7662207813
mohamad fazel <mdhesari99@gmail.com>
2020-04-03 12:58:59 +0430

    fix register problem showing button on desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,12 +12,30 @@ function sign_up() {
 
 function enterSignUp() {
   container.classList.add("right-panel-active");
-  document.getElementById("sign_up_btn").style.cssText="display:none";
-  document.getElementById("sign-in-btn").style.cssText="display:block";
+  // document.getElementById("sign_up_btn").style.cssText="display:none";
+  // document.getElementById("sign-in-btn").style.cssText="display:block";
 }
 
 function enterSignIn() {
   container.classList.remove("right-panel-active");
-  document.getElementById("sign_up_btn").style.cssText="display:block";
-  document.getElementById("sign-in-btn").style.cssText="display:none";
+  // document.getElementById("sign_up_btn").style.cssText="display:block";
+  // document.getElementById("sign-in-btn").style.cssText="display:none";
+}
+
+function enterSignUpOrSignIn() {
+  container.classList.toggle("right-panel-active");
+
+  let data = {
+    text: "Does not have any account?"
+  };
+
+  const is_sign_in = container.classList.contains("right-panel-active");
+
+  if (is_sign_in) {
+
+    data.text = "Already have an account?"
+  }
+  let register_action = document.getElementById("register-action");
+
+  register_action.innerHTML = data.text;
 }
